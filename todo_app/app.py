72776351513842282b3d04b31a92e0c72c642958
Andrea Savia <asavia3@gmail.com>
2020-11-18 23:30:52 +0000

    Added functionality to add new to-do item and refresh list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,7 @@
-from todo_app.data.session_items import get_items
-from flask import Flask, render_template
+from flask.globals import request
+from flask.helpers import url_for
+from todo_app.data.session_items import add_item, get_items
+from flask import Flask, render_template, redirect
 
 from todo_app.flask_config import Config
 
@@ -9,11 +11,15 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    # Modify the index() function to get the list of items and
-    # update the index.html template to display their titles as a list.
     items = get_items()
     return render_template('index.html', items=items)
 
 
+@app.route('/add', methods=['POST'])
+def add():
+    add_item(request.form.get('newItem'))
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
