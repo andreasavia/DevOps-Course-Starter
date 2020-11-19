f0eb1e8cb42f4ee9ad7123770cc0253292878fe7
Andrea Savia <asavia3@gmail.com>
2020-11-19 00:53:03 +0000

    Removed unused methods from app.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask.globals import request
 from flask.helpers import url_for
-from todo_app.data.session_items import add_item, get_items, get_item, save_item, complete_item
+from todo_app.data.session_items import add_item, get_items, complete_item
 from flask import Flask, render_template, redirect
 
 from todo_app.flask_config import Config
